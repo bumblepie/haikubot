7b8f9da2f00c928e6fc915d96eff24e6bb6ffb70
Bumblepie <luke@bumblepie.space>
2017-10-08 14:54:21 +1300

    Add more comprehensive tests to channel processor

--- discord-client/test/channelProcessor.js
+++ discord-client/test/channelProcessor.js
@@ -38,7 +38,7 @@ describe('ChannelProcessor', () => {
       assert.ok(onHaikuCalled);
     });
 
-    it('the arguments passed to onHaikuCalled should be the messages given to the processor', () => {
+    it('should pass the the messages given to the processor to onHaikuCalled', () => {
       assert.equal(haikuMessages.length, 0);
 
       const messages = [];
@@ -51,6 +51,93 @@ describe('ChannelProcessor', () => {
       });
 
       assert.deepEqual(haikuMessages, messages);
+    });
+
+    it('should not call onHaiku when a haiku is not found', () => {
+      assert.ok(!onHaikuCalled);
+
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+
+      assert.ok(!onHaikuCalled);
+    });
+
+    it('should find haikus when many messages are passed through', () => {
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      defaultProcessor.processMessage(sevenSyllableMessage);
+      defaultProcessor.processMessage(sevenSyllableMessage);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+
+      assert.ok(!onHaikuCalled);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      defaultProcessor.processMessage(sevenSyllableMessage);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      assert.ok(onHaikuCalled);
+
+      onHaikuCalled = false;
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      assert.ok(!onHaikuCalled);
+    });
+
+    it('should keep finding haikus after previously finding them', () => {
+      assert.ok(!onHaikuCalled);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      defaultProcessor.processMessage(sevenSyllableMessage);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      assert.ok(onHaikuCalled);
+      onHaikuCalled = false;
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      defaultProcessor.processMessage(sevenSyllableMessage);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      assert.ok(onHaikuCalled);
+      onHaikuCalled = false;
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      defaultProcessor.processMessage(sevenSyllableMessage);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      assert.ok(onHaikuCalled);
+    });
+
+    it('should even find a haiku that shares lines with a previous haiku', () => {
+      assert.ok(!onHaikuCalled);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      defaultProcessor.processMessage(sevenSyllableMessage);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      assert.ok(onHaikuCalled);
+      onHaikuCalled = false;
+      defaultProcessor.processMessage(sevenSyllableMessage);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      assert.ok(onHaikuCalled);
+    });
+
+    it('should not be interfered with if another processor is also receiving messages', () => {
+      const secondProcessor = new ChannelProcessor('channelID2');
+      secondProcessor.setOnHaikuFunction((messages) => {
+        onHaikuCalled = true;
+        haikuMessages = messages;
+      });
+
+      assert.ok(!onHaikuCalled);
+      defaultProcessor.processMessage(fiveSyllableMessage);
+
+      secondProcessor.processMessage(sevenSyllableMessage);
+      secondProcessor.processMessage(fiveSyllableMessage);
+      assert.ok(!onHaikuCalled);
+      defaultProcessor.processMessage(sevenSyllableMessage);
+
+      secondProcessor.processMessage(sevenSyllableMessage);
+      secondProcessor.processMessage(sevenSyllableMessage);
+      secondProcessor.processMessage(fiveSyllableMessage);
+      assert.ok(!onHaikuCalled);
+
+      defaultProcessor.processMessage(fiveSyllableMessage);
+      assert.ok(onHaikuCalled);
+      onHaikuCalled = false;
+      secondProcessor.processMessage(sevenSyllableMessage);
+      secondProcessor.processMessage(fiveSyllableMessage);
+      assert.ok(onHaikuCalled);
     });
   });
 });
