b3e3531e7ea95e8a04adc25dfd30fb9490ad030d
Bumblepie <luke@bumblepie.space>
2017-10-10 23:34:01 +1300

    Add formatHaiku test

--- discord-client/src/formatHaiku.js
+++ discord-client/src/formatHaiku.js
@@ -1,5 +1,5 @@
 exports.formatHaiku = haiku => `<@${haiku.author}> has created a beautiful Haiku!
-    "${haiku.lines[0]}
-    ${haiku.lines[1]}
-    ${haiku.lines[2]}"
-     - Haiku #${haiku.id}`;
+"${haiku.lines[0]}
+ ${haiku.lines[1]}
+ ${haiku.lines[2]}"
+ - Haiku #${haiku.id}`;
