6400c1172a5d1a3931740b4ac5fe3f1141f951cb
Bumblepie <luke@bumblepie.space>
2017-10-11 23:26:35 +1300

    Add tests for commands

--- discord-client/src/commands.js
+++ discord-client/src/commands.js
@@ -2,8 +2,12 @@ const { formatHaiku } = require('./formatHaiku');
 
 const commandMap = {};
 
-commandMap.getHaikuById = (context, id) => {
-  context.api.getHaikuById(id)
+commandMap.getHaikuById = (context, args) => {
+  if (args.length !== 1) {
+    throw Error('Invalid number of arguments for getHaikuById');
+  }
+  const id = args[0];
+  return context.api.getHaikuById(id)
     .then(responseHaiku => context.channel.send(formatHaiku(responseHaiku)))
     .catch((error) => {
       console.log(`Caught error ${error}, sending simplified error message to discord`);
@@ -15,8 +19,8 @@ exports.tryCommand = (context, args) => {
   const commandName = args[0];
   const commandArgs = args.slice(1);
   if (!(commandName in commandMap)) {
-    throw new Error(`Could not find command ${commandName}`);
+    throw Error(`Could not find command ${commandName}`);
   } else {
-    commandMap[commandName](context, commandArgs);
+    return commandMap[commandName](context, commandArgs);
   }
 };
