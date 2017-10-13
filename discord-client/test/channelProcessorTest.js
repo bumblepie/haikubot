84b5c043ed8cb977ccb5b02a347ef0bb5b32770f
Bumblepie <luke@bumblepie.space>
2017-10-13 23:05:49 +1300

    Add explanatory comment to channel processot test to show multiple non-unique authors test case covered

--- discord-client/test/channelProcessorTest.js
+++ discord-client/test/channelProcessorTest.js
@@ -57,6 +57,8 @@ describe('ChannelProcessor', () => {
 
       const fiveAuthor = fiveSyllableMessage.author;
       const sevenAuthor = sevenSyllableMessage.author;
+
+      //filters out second occurence of fiveauthor, but keeps both authors as they are unique
       const expectedHaiku = new Haiku(null, { lines, authors: [fiveAuthor.id, sevenAuthor.id] });
       assert.deepEqual(outputHaiku, expectedHaiku);
     });
