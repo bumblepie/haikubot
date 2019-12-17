340861842c653e17bff4ad38d5b8522b365a8948
bumblepie <luke@bumblepie.space>
2019-12-17 17:49:58 +1300

    Track existing cull messages to prevent race conditions

--- discord-client/src/commands/cull.js
+++ discord-client/src/commands/cull.js
@@ -3,11 +3,38 @@ const { isHaiku } = require('../validateHaiku');
 
 const REQUIRED_REACTIONS = 1;
 
+// Map of (server id -> serverCullingMap) for each server
+// serverCullingMap is an object with the form {
+//   candidates: set of haikus that haven't had a message open yet
+//   messages: map of (haiku id -> {channel id, message id}) of existing cull message for haiku
+// }
+const cullingMap = new Map();
+
+const updateCullingMap = async (context, serverId) => {
+  let serverCullingMap = cullingMap.get(serverId);
+
+  if (serverCullingMap == null) {
+    serverCullingMap = {
+      candidates: [],
+      messages: new Map(),
+    };
+  }
+
+  // Refetch when run out of candidates
+  if (serverCullingMap.candidates.length === 0) {
+    const haikus = await context.api.getHaikusInServer(serverId);
+    const cullingCandidates = haikus.filter(haiku => !isHaiku(haiku.lines));
+    serverCullingMap.candidates = cullingCandidates;
+  }
+  cullingMap.set(serverId, serverCullingMap);
+};
+
+
 const fetchHaikuToCull = async (context, serverId) => {
-  const haikus = await context.api.getHaikusInServer(serverId);
-  const cullingCandidates = haikus.filter(haiku => !isHaiku(haiku.lines));
+  await updateCullingMap(context, serverId);
+  const cullingCandidates = cullingMap.get(serverId).candidates;
   if (cullingCandidates.length > 0) {
-    return cullingCandidates[0];
+    return cullingCandidates.pop();
   }
   return null;
 };
@@ -52,15 +79,30 @@ exports.cullCommand = async (context, args) => {
   }
   const haikuId = haiku.id;
   try {
+    // Delete old culling message from map if it exists
+    const serverCullingMap = cullingMap.get(serverId);
+    if (serverCullingMap.messages.has(haikuId)) {
+      const { channelId, messageId } = serverCullingMap.messages.get(haikuId);
+      const oldMsg = await context.server.channels.get(channelId).fetchMessage(messageId);
+      // Ensure old message is deleted before new one is posted to prevent race conditions
+      await oldMsg.delete();
+    }
+
     // Show haiku
     const content = await formatHaiku(haiku, context.client, context.server);
     const message = await context.channel.send(content);
 
-    // await to ensure consistent order of emojis
+    // Await to ensure consistent order of emojis
     await message.react('👍');
     message.react('👎');
 
-    // Add message to message map so we can delete it when needed
+    // Add message to culling map so we can delete the message if it becomes stale
+    serverCullingMap.messages.set(haikuId, {
+      channelId: message.channel.id,
+      messageId: message.id,
+    });
+
+    // Add message to message map so we can cull/save the haiku based on reactions
     const deleteCallback = async () => context.api.deleteHaiku(context.server.id, haikuId);
     const saveCallback = async () => console.log('save haiku');
     const initialState = {
